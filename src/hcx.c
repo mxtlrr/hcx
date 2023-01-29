39589d25921d947b13c964856e0507a17ab36841
mxtlrr <ruson0914@gmail.com>
2023-01-29 10:42:49 -0600

    convert from spaces to tabs and work on some mouse events

--- src/hcx.c
+++ src/hcx.c
@@ -24,70 +24,77 @@ static void spawn(char **com) {
 		execvp((char*)com[0], (char**)com);
 		_exit(0);
 	}
-  wait(NULL);
+	wait(NULL);
 }
 
 int main(void){
-  Display* dpy;
-  XWindowAttributes attr;
-  XButtonEvent start;
-  XEvent ev;
+	Display* dpy;
+	XWindowAttributes attr;
+	XButtonEvent start;
+	XEvent ev;
 
-  if(!(dpy = XOpenDisplay(0x0))) return 1;
+	if(!(dpy = XOpenDisplay(0x0))) return 1;
 
-  // Grab ONLY:
-  // Win+F1 to Win+F3
-  // Win+Button1
-  // Win+Button3, which is RMB
-  XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), WIN, DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
-  XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F2")), WIN, DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
-  XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F3")), WIN, DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+	// Grab ONLY:
+	// Win+F1 to Win+F3
+	// Win+Button1
+	// Win+Button3, which is RMB
+	XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), WIN, DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+	XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F2")), WIN, DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+	XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F3")), WIN, DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
 
-  XGrabButton(dpy, LMB, WIN, DefaultRootWindow(dpy), True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-  XGrabButton(dpy, RMB, WIN, DefaultRootWindow(dpy), True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+	XGrabButton(dpy, LMB, WIN, DefaultRootWindow(dpy), True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+	XGrabButton(dpy, RMB, WIN, DefaultRootWindow(dpy), True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
 
-  start.subwindow = None;
-  for(;;){
-    // Poll through each event
-    XNextEvent(dpy, &ev);
+	start.subwindow = None;
+	for(;;){
+		// Poll through each event
+		XNextEvent(dpy, &ev);
 
-    // Raise window
-    if(ev.type == KeyPress && ev.xkey.subwindow != None){
-      printf("%d\n", ev.xkey.keycode);
-      unsigned int keycode = ev.xkey.keycode;
-      switch(keycode){
-        case 67: // F1
-          // Raise window only in this case
-          XRaiseWindow(dpy, ev.xkey.subwindow);
-          break;
+		// Raise window
+		if(ev.type == KeyPress && ev.xkey.subwindow != None){
+			printf("%d\n", ev.xkey.keycode);
+			unsigned int keycode = ev.xkey.keycode;
+			switch(keycode){
+				case 67: // F1
+					// Raise window only in this case
+					XRaiseWindow(dpy, ev.xkey.subwindow);
+					break;
 
-        case 68: // F2
-          spawn(termcmd);
-          break;
+				case 68: // F2
+					spawn(termcmd);
+					break;	
 
-        case 69: // F3
-          spawn(menucmd);
-          break;
-      }
-    }
+				case 69: // F3
+					spawn(menucmd);
+					break;
+			}
+		}
 
-    if(ev.type == ButtonPress && ev.xbutton.subwindow != None){
-      XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
-      start = ev.xbutton;
-    } 
-    
-    // This event only fires when we are clicking and dragging at the same time.
-    // We also have to check if we are on the current window
-    if(ev.type == MotionNotify && start.subwindow != None){
-      int xdiff = ev.xbutton.x_root - start.x_root;
-      int ydiff = ev.xbutton.y_root - start.y_root;
-      XMoveResizeWindow(dpy, start.subwindow,
-        attr.x + (start.button==1 ? xdiff : 0),
-        attr.y + (start.button==1 ? ydiff : 0),
-        MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
-        MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
-    }
+		if(ev.type == ButtonPress && ev.xbutton.subwindow != None){
+			XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
+			start = ev.xbutton;
+		} else {
+			// We are on the root window
+			if(ev.type == ButtonPress &&	ev.xbutton.window == DefaultRootWindow(dpy)){
+				unsigned int button = ev.xbutton.button;
+				printf("unhandeled button: %d\n", button);
+				// switch(button)
+			}
+		}
+		
+		// This event only fires when we are clicking and dragging at the same time.
+		// We also have to check if we are on the current window
+		if(ev.type == MotionNotify && start.subwindow != None){
+			int xdiff = ev.xbutton.x_root - start.x_root;
+			int ydiff = ev.xbutton.y_root - start.y_root;
+			XMoveResizeWindow(dpy, start.subwindow,
+				attr.x + (start.button==1 ? xdiff : 0),
+				attr.y + (start.button==1 ? ydiff : 0),
+				MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
+				MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
+		}
 
-    if(ev.type == ButtonRelease) start.subwindow = None;
-  }
+		if(ev.type == ButtonRelease) start.subwindow = None;
+	}
 }
